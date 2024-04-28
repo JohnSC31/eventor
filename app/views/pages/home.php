3f4b668acbca685283a0e43e42ade539c7269ffb
johnS31 <jostsace05@gmail.com>
2024-04-27 18:47:25 -0600

    Diseño Inicio
    Se completa el diseño de la pagina de inicio

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -36,19 +36,49 @@
 
 <section class="home-services">
     <div class="container">
-        <h2>Servicios</h2>
-        <div class="services-list">
-            <div class="service-detail-container">
+        <h2>Eventos</h2>
+        <div class="event-list">
+            
+            <div class="event-detail-container">
                 <div class="event-detail">
+                    <h3>Cenas</h3>
+                    <p>Estrecha relaciones con tus clientes o mejora el ambiente de tu empresa con una cena de calidad 
+                        sin preocuparte por los detalles.
+                    </p>
+                </div>  
+            </div><!-- .event-detail-container -->
 
-                </div>
-                <ul class="event-icon-container">
-                    <li class=""><i class="fa-solid fa-utensils"></i></li>
-                    <li class=""><i class="fa-solid fa-utensils"></i></li>
-                    <li class=""><i class="fa-solid fa-utensils"></i></li>
-                    <li class=""><i class="fa-solid fa-utensils"></i></li>
-                </ul>
-            </div>
+            <ul class="event-icon-container">
+                <li class=""><i class="fa-solid fa-utensils"></i></li>
+                <li class=""><i class="fa-solid fa-utensils"></i></li>
+                <li class=""><i class="fa-solid fa-utensils"></i></li>
+                <li class=""><i class="fa-solid fa-utensils"></i></li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+<section class="our-team">
+    <div class="container">
+        <h2>Nuestro Equipo</h2>
+        <div class="team-container">
+            <div class="team-member" id="team-member-1">
+                <h3>John Sánchez</h3>
+                <div class="translucent-layer"></div>
+                
+            </div><!-- .team-member -->
+
+            <div class="team-member" id="team-member-2">
+                <h3>Edu Gonzales</h3>
+                <div class="translucent-layer"></div>
+                
+            </div><!-- .team-member -->
+
+            <div class="team-member" id="team-member-3">
+                <h3>Victoria Sandí</h3>
+                <div class="translucent-layer" ></div>
+                
+            </div><!-- .team-member -->
         </div>
     </div>
 </section>
